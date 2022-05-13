0831df5ed7b3e860bb4dfcae02354f85629ac212
Kristian Aleksiev <103513324+KristianAleksiev@users.noreply.github.com>
2022-05-13 21:06:15 +0300

    Add files via upload

---
+++ Day_30/json_data.py
@@ -0,0 +1,15 @@
+import json
+
+#Write
+json.dump()
+
+#Load
+json.load()
+
+#Update
+json.update()
+
+#with open("generated_passwords.json", mode="r") as passwords_file:
+    # json.dump(new_data, passwords_file, indent=4) #=> Writing to a json file, "what", "document name", indentation
+    # data = json.load(passwords_file) #Reading the file
+    # print(data) #=> Returns dictionary format
