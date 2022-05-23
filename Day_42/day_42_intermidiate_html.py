19feb71e09720e1fb57f8b537eee3ad848e75a98
Kristian Aleksiev <103513324+KristianAleksiev@users.noreply.github.com>
2022-05-23 17:18:41 +0300

    Add files via upload

---
+++ Day_42/day_42_intermidiate_html.py
@@ -0,0 +1,19 @@
+### Day 42 - Intermediate HTML
+
+## HTML Tables
+#<table> </table>
+#<tr> - Table row </tr>
+#<thead> - Table head </th> - Bolded by default <th> Table header cell </th>
+#<td> - Table data (cell) </td>
+
+
+## Deprecated attributes - HTML elements for visualisation -> CSS
+
+
+## HTML Forms
+#<form class="" action="index.html" method="post">
+#<label>Your name:</label>
+#      <input type="text" name="" value="">
+#      <input type="submit" name="">
+#
+# </form>
