311f15d81b18e9e323cc90a277e5f7c7c6be408b
Kristian Aleksiev <103513324+KristianAleksiev@users.noreply.github.com>
2022-05-23 17:16:02 +0300

    Add files via upload

---
+++ Day_41/day_41_introduction_html.py
@@ -0,0 +1,34 @@
+### Day 41 - Introduction to HTML - Creating websites with html, CSS
+
+#Home -> IPS -> DNS server -> IP -> Home -> IPS -> Internet backbone -> Server at IP adress -> IPS -> Home
+### Types of files -> html, css, JS
+
+## HTML files -> Responsible for the structure of the website </> - HyperText MARKUP Language
+## CSS files -> Responsible for website styling {..}
+## JavaScript -> Responsible for functionality of the website </>
+
+#devdocs.io -> Documentation
+
+###HTML - Self-closing tags, open-close tags, HTML Attribute(specified modifications)
+
+#<h1> Level 1 heading using h1 tag </h1> #From 1-6, lowers size
+#<br> - Line break - <!-- Comment in html --> Spacing
+#<p> Paragraph formatting </p>
+#<em>  </em> Emphasis tag ->
+#<i> Italicyse </i>
+#<strong> Bigger attention  </strong>
+# Metatags - <meta charset="utf-8"> - All text encoded using utf-8 system - utf-8 standard ### Search Engine ###
+
+### HTML Lists
+##<ul> - Unordered lists, Ordered lists
+#       <li>first item</li>
+#       <li>second item</li>
+#       <li>third item</li>
+#</ul>
+
+### HTML Images
+## <img src="pic.png" alt="Something else"> #### SEO
+
+### HTML Anchortags -Hyperlinks
+##<a href="https:/python.org">Python<a/> => <a href="adress"> TEXT </a>
+
