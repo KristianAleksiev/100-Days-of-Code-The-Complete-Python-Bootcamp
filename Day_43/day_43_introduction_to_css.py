8e6d10b4328003c76d7b4db79b3b37b55c6bb56d
Kristian Aleksiev <103513324+KristianAleksiev@users.noreply.github.com>
2022-05-24 16:59:31 +0300

    Add files via upload

---
+++ Day_43/day_43_introduction_to_css.py
@@ -0,0 +1,24 @@
+### Day 43 - Web foundation - Introduction to CSS - Cascading style sheets
+
+### Inline css, External css files
+##Inline element - Higher priority -> Internal css -> External css (Good practice)
+
+### Debugging css code
+
+### CSS Syntax
+
+##  tag selector {property: value;} -> Who {What: How;} -> h1{color: red;} - Alphabetical order
+
+## class selectors - > <img class="NAME" src=....> /* */ open close comment
+# An element can have more than one class "class="CLASS_NAME1 CLASS_NAME2"
+# .NAME{property: value}
+
+#Pseudo-class img:hover {property: value}
+
+## ID selector
+# <h1 id="heading">
+# #heading {property: value;}
+
+### The more specific the more priority it has
+## ID selectors are used when one element will be modified,
+## The class selectors are used when more than one element will be modified
